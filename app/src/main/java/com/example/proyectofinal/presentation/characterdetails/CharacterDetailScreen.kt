e442eb54ab976a633ae94ab95febb520b8bb58b0
Pedro Martinez <pedro.martinez@gruposantander.com>
2023-08-11 09:57:27 +0200

    move showerror and clean imports

--- app/src/main/java/com/example/proyectofinal/presentation/characterdetails/CharacterDetailScreen.kt
+++ app/src/main/java/com/example/proyectofinal/presentation/characterdetails/CharacterDetailScreen.kt
@@ -2,7 +2,6 @@ package com.example.proyectofinal.presentation.characterdetails
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,11 +14,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,13 +25,10 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
 import coil.compose.AsyncImage
 import com.example.proyectofinal.R
 import com.example.proyectofinal.domain.model.CharacterModel
-import com.example.proyectofinal.presentation.ShowError
-import com.example.proyectofinal.presentation.components.StarComponent
-import com.example.proyectofinal.presentation.components.StarIcon
+import com.example.proyectofinal.presentation.components.ShowError
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
--- app/src/main/java/com/example/proyectofinal/presentation/components/ShowError.kt
+++ app/src/main/java/com/example/proyectofinal/presentation/components/ShowError.kt
@@ -1,4 +1,4 @@
-package com.example.proyectofinal.presentation
+package com.example.proyectofinal.presentation.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
