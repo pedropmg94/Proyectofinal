b1861e513574b91fc26e3a758ea5e165e182a2d5
pedropmg94 <39901728+pedropmg94@users.noreply.github.com>
2024-10-12 21:41:34 +0200

    Remove old star xml and add new one with compose (#2)

--- app/src/main/java/com/example/proyectofinal/presentation/components/CardItem.kt
+++ app/src/main/java/com/example/proyectofinal/presentation/components/CardItem.kt
@@ -1,21 +1,21 @@
 package com.example.proyectofinal.presentation.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -24,7 +24,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
 import coil.compose.AsyncImage
 import com.example.proyectofinal.R
 import com.example.proyectofinal.domain.model.CharacterModel
@@ -58,7 +57,8 @@ fun <T : Any> CardItem(
                 modifier = Modifier
                     .padding(8.dp)
                     .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 CharacterName(nameProvider(item))
 
@@ -99,26 +99,25 @@ fun ItemImage(itemName: String, itemPhotoURL: String) {
 }
 
 @Composable
-fun StarIcon(favModel: FavModel, favClick: (FavModel) -> Unit) {
-
-    var starred by remember {
-        mutableStateOf(favModel.favorite)
-    }
-
-    AndroidView(
-        modifier = Modifier.clickable {
-            starred = !starred
+private fun StarIcon(
+    favModel: FavModel,
+    favClick: (FavModel) -> Unit
+) {
+    IconButton(
+        onClick = {
             favClick(favModel.copy(favorite = !favModel.favorite))
-        },
-        factory = { context ->
-            StarComponent(context).apply {
-                this.checked = starred
-            }
-        },
-        update = {
-            it.checked = starred
         }
-    )
+    ) {
+        Icon(
+            painter = if (favModel.favorite) {
+                painterResource(R.drawable.star_filled)
+            } else {
+                painterResource(R.drawable.star)
+            },
+            contentDescription = null,
+            modifier = Modifier.size(35.dp)
+        )
+    }
 }
 
 @Composable
