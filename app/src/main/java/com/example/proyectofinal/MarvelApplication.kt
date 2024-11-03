780a39aa554569d1e517da19f49296244c15ee70
Pedro <pedropmg94@gmail.com>
2024-11-03 13:38:42 +0100

    update kotlin and compose

--- app/src/main/java/com/example/proyectofinal/MarvelApplication.kt
+++ app/src/main/java/com/example/proyectofinal/MarvelApplication.kt
@@ -4,28 +4,14 @@ import android.app.Application
 import com.example.proyectofinal.di.dataModule
 import com.example.proyectofinal.di.domainModule
 import com.example.proyectofinal.di.presentationModule
-import org.koin.android.BuildConfig
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
 
 class MarvelApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
-            /*
-            En modo debug (depuración) mostraré mensajes por consola y de lo contrario no lo haré
-             */
-            androidLogger(
-                if (BuildConfig.DEBUG) {
-                    Level.INFO
-                } else {
-                    Level.NONE
-                }
-            )
-
             androidContext(this@MarvelApplication)
 
             modules(
