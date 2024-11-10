2efb8c6493a201d7f80773a8210fb0fec8630131
pedropmg94 <39901728+pedropmg94@users.noreply.github.com>
2024-11-10 13:46:38 +0100

    implement independent ComicListScreen and ComicListViewModel (#11)

--- app/src/main/java/com/example/proyectofinal/presentation/screens/comiclist/ComicListScreen.kt
+++ app/src/main/java/com/example/proyectofinal/presentation/screens/comiclist/ComicListScreen.kt
@@ -5,27 +5,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.proyectofinal.domain.model.ComicModel
+import com.example.proyectofinal.presentation.common.Action
 import com.example.proyectofinal.presentation.common.ScreenUIState2
 import com.example.proyectofinal.presentation.common.components.CardItem
 import com.example.proyectofinal.presentation.common.components.ErrorView
 import com.example.proyectofinal.presentation.common.components.LoadingView
 import com.example.proyectofinal.presentation.common.components.ScaffoldTopBar
-import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun ComicListScreen(
-    viewModel: ComicListViewModel = koinViewModel(),
+    state: ComicListState,
     //onItemClick: (Int) -> Unit
-    onTabItem: (Int) -> Unit
+    onTabItem: (Int) -> Unit,
+    onActions: (Action) -> Unit
 ) {
-    val state by viewModel.state.observeAsState(ComicListState())
-    
     ScaffoldTopBar(
         onTabClick = {
             onTabItem(it)
@@ -44,20 +41,21 @@ fun ComicListScreen(
 
                 is ScreenUIState2.Error -> {
                     ErrorView(
-                        error = (state.comicUIState as ScreenUIState2.Error).error,
-                        onClickRetry = { viewModel.retryComic() }
+                        error = state.comicUIState.error,
+                        onClickRetry = {
+                            onActions(
+                                ComicListScreenAction.OnTryAgainClick
+                            )
+                        }
                     )
                 }
             }
         }
-
     }
 }
 
 @Composable
 fun ContentComicList(
-    //onItemClick: (Int) -> Unit,
-    //onTabItem: (Int) -> Unit
     comicList: List<ComicModel>
 ) {
     LazyColumn(
@@ -79,8 +77,9 @@ fun ContentComicList(
 @Composable
 fun MovieListScreenPreview() {
     ComicListScreen(
-        //onItemClick = {},
-        onTabItem = {}
+        state = ComicListState(),
+        onTabItem = {},
+        onActions = {}
     )
 }
 
