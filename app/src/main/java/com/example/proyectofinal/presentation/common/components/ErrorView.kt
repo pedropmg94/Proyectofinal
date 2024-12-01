64c1a864801b8c4c9dd162b590a45826031087bc
Pedro <pedropmg94@gmail.com>
2024-12-01 20:58:06 +0100

    improve ErrorView

--- app/src/main/java/com/example/proyectofinal/presentation/common/components/ErrorView.kt
+++ app/src/main/java/com/example/proyectofinal/presentation/common/components/ErrorView.kt
@@ -1,33 +1,48 @@
 package com.example.proyectofinal.presentation.common.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ErrorView(
     error: String,
     onClickRetry: () -> Unit
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
         Text(
             text = error,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .align(Alignment.Center),
             color = Color.White
         )
         Button(
-            onClick = { onClickRetry() }
+            onClick = { onClickRetry() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .align(Alignment.BottomCenter),
+            shape = RoundedCornerShape(16.dp),
+            border = BorderStroke(2.dp, Color.Black),
+            colors = ButtonDefaults.buttonColors(Color.White)
         ) {
-            Text(text = "Try again")
+            Text(text = TryAgain)
         }
     }
 }
+
+private const val TryAgain = "TRY AGAIN"
